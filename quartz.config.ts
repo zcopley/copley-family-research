101fa7b1213f3e62109e8bdebab280cccfdfd006
zcopley <zcopley@users.noreply.github.com>
2026-04-19 05:12:25 +0000

    deploy: 7c1b8d6ae335e9edcfa72503a619d43f3dc84d1b

--- quartz.config.ts
+++ quartz.config.ts
@@ -98,8 +98,7 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      // Comment out CustomOgImages to speed up build time
-      // Plugin.CustomOgImages(),
+      Plugin.CustomOgImages(),
     ],
   },
 }
