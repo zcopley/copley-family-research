cd7493f197cde6e9618bbbe85713beba6cb3bc52
zcopley <zcopley@users.noreply.github.com>
2026-04-19 04:52:57 +0000

    deploy: f7218259ed4c22fe1d3f52c43fccdc6f0667977d

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/zcopley/copley-family-research",
+      "Contact": "mailto:zcopley@gmail.com",
     },
   }),
 }
